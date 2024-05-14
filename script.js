78bbc2d9fbe81f12713a351256643333c75a136b
Abhay Singh <iabhaysingh3@gmail.com>
2024-05-14 22:27:09 +0530

    added javascript codes

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
+
 
 function calculateAge(){
-    let bithDate = new Date(userInput.value);
+    let birthDate = new Date(userInput.value);
 
     let d1 = birthDate.getDate();
     let m1 = birthDate.getMonth() + 1;
-    let y1 = brithDate.getFullYear();
-
+    let y1 = birthDate.getFullYear();
+    
     let today = new Date();
-
+    
     let d2 = today.getDate();
     let m2 = today.getMonth() + 1;
     let y2 = today.getFullYear();
-
+    
     let d3, m3, y3;
-
+    
     y3 = y2 - y1;
-
+    
     if(m2 >= m1) {
         m3 = m2 - m1;
     }else{
@@ -31,6 +33,11 @@ function calculateAge(){
         m3--;
         d3 = getDaysInMonth(y1, m1) + d2 -d1;
     }
+    if(m3 < 0) {
+        m3 = 11;
+        y--;
+    }
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3} </span>months, and <span>${d3}</span> days old`;
 }
 
 function getDaysInMonth(year, month){
